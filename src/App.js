275d3f6bb7e4f376a3b9d864ee53d53c42973e10
Dimamitosan <shagarov07@gmail.com>
2024-02-13 17:49:46 +0300

    ready react shop

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import items from './items'
+import Items from './components/Items'
+import Categories from './components/Categories'
+import ShowFullItem from './components/ShowFullItem'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      currentItems: [],
+      items,
+      showFullItem: false,
+      fullItem: {},
+    }
+    this.state.currentItems = this.state.items
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items
+          onShowItem={this.onShowItem}
+          items={this.state.currentItems}
+          onAdd={this.addToOrder}
+        />
+        {this.state.showFullItem && (
+          <ShowFullItem
+            item={this.state.fullItem}
+            onShowItem={this.onShowItem}
+            onAdd={this.addToOrder}
+          />
+        )}
+        <Footer />
+      </div>
+    )
+  }
+
+  onShowItem(item) {
+    this.setState({ fullItem: item })
+    this.setState({ showFullItem: !this.state.showFullItem })
+  }
+
+  chooseCategory(category) {
+    if (category === 'all') {
+      this.setState({ currentItems: this.state.items })
+      return
+    }
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    })
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) })
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) isInArray = true
+    })
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] })
+  }
+}
+
+export default App
---
+++ src/items.js
@@ -0,0 +1,36 @@
+let items = [
+  {
+    id: 1,
+    title: 'Стул серый',
+    img: 'chair-grey.jpeg',
+    desc: 'lorem dpsoai pasjd pj dsapqwpoj mdpj',
+    category: 'chairs',
+    price: '49.99',
+  },
+  {
+    id: 2,
+    title: 'Стол белый',
+    img: 'table-white.webp',
+    desc: 'lorem dpsoai pasjd pj dsapqwpoj mdpj',
+    category: 'tables',
+    price: '59.99',
+  },
+  {
+    id: 3,
+    title: 'Кресло белое',
+    img: 'armchair-white.jpg',
+    desc: 'lorem dpsoai pasjd pj dsapqwpoj mdpj',
+    category: 'armchairs',
+    price: '65.99',
+  },
+  {
+    id: 4,
+    title: 'Стул синий',
+    img: 'chair-blue.jpg',
+    desc: 'lorem dpsoai pasjd pj dsapqwpoj mdpj',
+    category: 'chairs',
+    price: '32.99',
+  },
+]
+
+export default items
